7398e7db2d33ca8cff0381afce2e900efe8be737
JonSimonsen <jonsimonsen@users.noreply.github.com>
2020-06-05 11:01:04 +0200

    Added Tetrimino class to JS. Committing before testing/debugging.

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 $(document).ready(function() {
 
-  // ----------------------------\
-  // Initialization of game board\
-  // ----------------------------\
+  // -----------------------\
+  // Variable initialization\
+  // -----------------------\
 
-  // Create grid
+  // Grid
   const height = 20;
   const width = 12;     //Includes walls
   const startRow = -3;  //Number of invisible rows above the game board (as a negative number)
-
-// -----------------------\
-// Variable initialization\
-// -----------------------\
-
   let gameContainer;
   let squares;
 
@@ -23,8 +18,7 @@ $(document).ready(function() {
   let startSpeed = 400;
   let maxSpeed = 80;    //highest speed before going to killSpeed
   let killSpeed = 40;   //final level speed
-  const speedDip = gameSpeed / 10;
-  const colors = ['purple', 'orange', 'gray'];
+  const speedDip = startSpeed / 10;
 
   //Scoring
   const baseScore = 100800;
@@ -32,69 +26,123 @@ $(document).ready(function() {
   let lines;
   const linesPerLevel = 1;
 
-  //Tetriminoes
-  const jTetrimino = [
-    [width, width + 1, width + 2, width * 2 + 2],
-    [1, width + 1, width * 2, width * 2 + 1],
-    [0, width, width + 1, width + 2],
-    [1, 2, width + 1, width * 2 + 1]
-  ];
 
-  const lTetrimino = [
-    [width, width + 1, width + 2, width * 2],
-    [0, 1, width + 1, width * 2 + 1],
-    [2, width, width + 1, width + 2],
-    [1, width + 1, width * 2 + 1, width * 2 + 2]
-  ];
+  class Tetrimino {
+    //Class specific constants
+    const _startPos = 4;
+    const _startRotation = 0;
+    const _colors = ['purple', 'orange', 'gray'];
 
-  const tTetrimino = [
-    [width, width + 1, width + 2, width * 2 + 1],
-    [1, width, width + 1, width * 2 + 1],
-    [1, width, width + 1, width + 2],
-    [1, width + 1, width + 2, width * 2 + 1]
-  ];
+    //Tetriminoes
+    const _jTetrimino = [
+      [width, width + 1, width + 2, width * 2 + 2],
+      [1, width + 1, width * 2, width * 2 + 1],
+      [0, width, width + 1, width + 2],
+      [1, 2, width + 1, width * 2 + 1]
+    ];
 
-  const zTetrimino = [
-    [width, width + 1, width * 2 + 1, width * 2 + 2],
-    [2, width + 1, width + 2, width * 2 + 1],
-    [width, width + 1, width * 2 + 1, width * 2 + 2],
-    [2, width + 1, width + 2, width * 2 + 1]
-  ];
+    const _zTetrimino = [
+      [width, width + 1, width * 2 + 1, width * 2 + 2],
+      [2, width + 1, width + 2, width * 2 + 1],
+      [width, width + 1, width * 2 + 1, width * 2 + 2],
+      [2, width + 1, width + 2, width * 2 + 1]
+    ];
 
-  const sTetrimino = [
-    [width + 1, width + 2, width * 2, width * 2 + 1],
-    [1, width + 1, width + 2, width * 2 + 2],
-    [width + 1, width + 2, width * 2, width * 2 + 1],
-    [1, width + 1, width + 2, width * 2 + 2]
-  ];
+    const _lTetrimino = [
+      [width, width + 1, width + 2, width * 2],
+      [0, 1, width + 1, width * 2 + 1],
+      [2, width, width + 1, width + 2],
+      [1, width + 1, width * 2 + 1, width * 2 + 2]
+    ];
 
-  const iTetrimino = [
-    [width * 2, width * 2 + 1, width * 2 + 2, width * 2 + 3],
-    [2, width + 2, width * 2 + 2, width * 3 + 2],
-    [width * 2, width * 2 + 1, width * 2 + 2, width * 2 + 3],
-    [2, width + 2, width * 2 + 2, width * 3 + 2]
-  ];
+    const _sTetrimino = [
+      [width + 1, width + 2, width * 2, width * 2 + 1],
+      [1, width + 1, width + 2, width * 2 + 2],
+      [width + 1, width + 2, width * 2, width * 2 + 1],
+      [1, width + 1, width + 2, width * 2 + 2]
+    ];
 
-  const oTetrimino = [
-    [width + 1, width + 2, width * 2 + 1, width * 2 + 2],
-    [width + 1, width + 2, width * 2 + 1, width * 2 + 2],
-    [width + 1, width + 2, width * 2 + 1, width * 2 + 2],
-    [width + 1, width + 2, width * 2 + 1, width * 2 + 2]
-  ];
+    const _tTetrimino = [
+      [width, width + 1, width + 2, width * 2 + 1],
+      [1, width, width + 1, width * 2 + 1],
+      [1, width, width + 1, width + 2],
+      [1, width + 1, width + 2, width * 2 + 1]
+    ];
 
-  const tetriminoes = [jTetrimino, lTetrimino, tTetrimino, zTetrimino, sTetrimino, iTetrimino, oTetrimino];
+    const _iTetrimino = [
+      [width * 2, width * 2 + 1, width * 2 + 2, width * 2 + 3],
+      [2, width + 2, width * 2 + 2, width * 3 + 2],
+      [width * 2, width * 2 + 1, width * 2 + 2, width * 2 + 3],
+      [2, width + 2, width * 2 + 2, width * 3 + 2]
+    ];
+
+    const _oTetrimino = [
+      [width + 1, width + 2, width * 2 + 1, width * 2 + 2],
+      [width + 1, width + 2, width * 2 + 1, width * 2 + 2],
+      [width + 1, width + 2, width * 2 + 1, width * 2 + 2],
+      [width + 1, width + 2, width * 2 + 1, width * 2 + 2]
+    ];
+
+    const _tetriminoes = [_jTetrimino, _zTetrimino, _lTetrimino, _sTetrimino, _tTetrimino, _iTetrimino, _oTetrimino];
+
+    //Construct a new piece
+    constructor() {
+      this._position = _startPos;
+      this._rotation = _startRotation;
+      this._makePiece();
+    }
+
+    //Helper for the constructor (set _piece and _color based on a random selection)
+    _makePiece() {
+      let selection = Math.floor(Math.random() * tetriminoes.length);
+      if(floor(selection / 2) === 0){
+        this._color = this._colors[0];
+      }
+      else if(floor(selection / 2) === 1){
+        this._color = this._colors[1];
+      }
+      else{
+        this._color = this._colors[2];
+      }
+      this._piece = this._tetriminoes[selection];
+    }
+
+    /*Getters and setters*/
+    get position() {
+      return this._position;
+    }
+    set position(pos) {
+      this._position = pos;
+    }
+    get rotation() {
+      return this._rotation;
+    }
+    set rotation(rot) {
+      this._rotation = rot;
+    }
+    get color() {
+      return this._color;
+    }
+    //Get the piece in its current rotation
+    get piece() {
+      return this._piece[this._rotation];
+    }
+
+    /*Other methods*/
+
+    //Get the piece in a different rotation
+    getNewPiece(rot) {
+      return this._piece[rot];
+    }
+  }
 
   // ---------\
   // Main Code\
   // ---------\
 
   //Create a tetrimino at the top (would make sense to use classes eventually)
-  let currentColor;
   let current;
-  let nextColor;
   let next;
-  let pos;
-  let rot;
 
   newGame();
 
@@ -158,7 +206,7 @@ $(document).ready(function() {
     paused = true;
     timerId = null;
     gameSpeed = startSpeed;
-    next = makePiece();
+    next = new Tetrimino();
 
     showNext();
     spawn();
@@ -167,19 +215,20 @@ $(document).ready(function() {
   }
 
   //Create a new tetrimino
-  function makePiece() {
+  /*function makePiece() {
     let selection = Math.floor(Math.random() * tetriminoes.length);
-    if(selection === 0 || selection === 3){
-      nextColor = colors[0];
+    let color;
+    if(floor(selection / 2) === 0){
+      color = colors[0];
     }
-    else if(selection === 1 || selection === 4){
-      nextColor = colors[1];
+    else if(floor(selection / 2) === 1){
+      color = colors[1];
     }
     else{
-      nextColor = colors[2];
+      color = colors[2];
     }
-    return tetriminoes[selection];
-  }
+    return Tetrimino(tetriminoes[selection], color);
+  }*/
 
   //Create a new tetrimino
   function spawn() {
@@ -187,12 +236,9 @@ $(document).ready(function() {
     gameContainer = document.getElementsByClassName("grid")[0];
     squares = Array.from(document.querySelectorAll('.grid div'));
 
-    pos = 4;
-    rot = 0;
     current = next;
-    currentColor = nextColor;
     gameOver();
-    next = makePiece();
+    next = new Tetrimino();
     draw();
     if(!paused){
       showNext();
@@ -205,7 +251,7 @@ $(document).ready(function() {
     shownSquares.forEach(field => {
       field.classList.remove('next');
     })
-    next[0].forEach( index => {
+    next.piece.forEach( index => {
       let tmp = index;
       if(tmp >= width && tmp < 2 * width){
         tmp += 4 - width;
@@ -219,15 +265,15 @@ $(document).ready(function() {
 
   //draw tetrimino
   function draw() {
-    current[rot].forEach(filled => {
-      squares[filled + pos].classList.add('tetrimino', currentColor);
+    current.piece.forEach(filled => {
+      squares[filled + current.position].classList.add('tetrimino', current.color);
     })
   }
 
   //undraw tetrimino
   function undraw() {
-    current[rot].forEach(filled => {
-      squares[filled + pos].classList.remove('tetrimino', currentColor);
+    current.piece.forEach(filled => {
+      squares[filled + current.position].classList.remove('tetrimino', current.color);
     })
   }
 
@@ -261,7 +307,7 @@ $(document).ready(function() {
   //Make tetrimino descend
   function descend() {
     undraw();
-    pos += width;
+    current.position += width;
     draw();
     rest();
   }
@@ -269,8 +315,8 @@ $(document).ready(function() {
   //Stop if tetrmino lands on the floor or previously placed tetriminoes)
   function rest() {
     //Optimally, this should happen just before the tetrimino moves to the next position
-    if(current[rot].some(index => squares[pos + index + width].classList.contains('occupied') || squares[pos + index + width].classList.contains('wall'))) {
-      current[rot].forEach(index => squares[pos + index].classList.add('occupied'));
+    if(current.piece.some(index => squares[current.position + index + width].classList.contains('occupied') || squares[current.position + index + width].classList.contains('wall'))) {
+      current.piece.forEach(index => squares[current.position + index].classList.add('occupied'));
 
       //Clean up game board and update score
       update();
@@ -282,38 +328,39 @@ $(document).ready(function() {
 
   //Move left if possible
   function moveLeft() {
-    if(!(current[rot].some(index => squares[pos + index - 1].classList.contains("occupied"))) && !(current[rot].some(index => squares[pos + index - 1].classList.contains("wall")))) {
+    if(!(current.piece.some(index => squares[current.position + index - 1].classList.contains("occupied"))) && !(current.piece.some(index => squares[current.position + index - 1].classList.contains("wall")))) {
       undraw();
-      pos--;
+      current.position--;
       draw();
     }
   }
 
   //Move right if possible
   function moveRight() {
-    if(!(current[rot].some(index => squares[pos + index + 1].classList.contains("occupied"))) && !(current[rot].some(index => squares[pos + index + 1].classList.contains("wall")))) {
+    if(!(current.piece.some(index => squares[current.position + index + 1].classList.contains("occupied"))) && !(current.piece.some(index => squares[current.position + index + 1].classList.contains("wall")))) {
       undraw();
-      pos++;
+      current.position++;
       draw();
     }
   }
 
   //Rotate if possible (quarters should tells the number of 90 degree clockwise rotations)
   function rotate(quarters) {
-    tmp = (rot + quarters) % 4;
+    let tmp = (current.rotation + quarters) % 4;
+    let newPiece = current.getNewPiece(tmp)
 
     //Confirm that all squares of the rotated tetrimino inhabit unoccupied spaces.
-    if(!(current[tmp].some(index => squares[pos + index].classList.contains("occupied"))) && !(current[tmp].some(index => squares[pos + index].classList.contains("wall")))) {
+    if(!(newPiece.some(index => squares[current.position + index].classList.contains("occupied"))) && !(newPiece.some(index => squares[current.position + index].classList.contains("wall")))) {
       undraw();
-      rot = tmp;
+      current.rotation = tmp;
       draw();
     }
   }
 
   //Update game board and fix score if necessary
   function update(){
-    let firstRow = Math.floor((pos + current[rot][0]) / width);
-    let lastRow = Math.floor((pos + current[rot][3]) / width);
+    let firstRow = Math.floor((current.position + current.piece[0]) / width);
+    let lastRow = Math.floor((current.position + current.piece[3]) / width);
     let fullRows = 0;
     for(let i = lastRow; i >= firstRow; i--){
       let full = true;
@@ -393,7 +440,7 @@ $(document).ready(function() {
 
   //Game over check
   function gameOver(){
-    if(current[rot].some(index => squares[pos + index].classList.contains("occupied"))){
+    if(current.piece.some(index => squares[current.position + index].classList.contains("occupied"))){
       $(".game-state").text("Game Over");
       $("#start-button").text("Try again");
       $("#start-button").attr("class", "dead");
